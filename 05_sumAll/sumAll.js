89b1ad57b1657f90de5e669c8e1c95ed51a1eeb8
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-05 00:00:05 +1000

    Finished 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0){
+        return "ERROR";
+    }
+    if(a % 1 !== 0 || b % 1 !== 0 ){
+        return "ERROR";
+    }
+    if(typeof(a) !== "number" || typeof(b) !== "number"){
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    const high = Math.max(a,b);
+    let low = Math.min(a,b);
+    for(;low <= high; low++){
+        sum += low;
+    }
+    return sum;
 };
 
 // Do not edit below this line
