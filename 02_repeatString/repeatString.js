eeb8ce94acab7859ea182ea67106fe9b896fb233
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-04 23:12:54 +1000

    Finished 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(sentence, count) {
+    if (count < 0){
+        return "ERROR";
+    }
+    let newWord = "";
+    for (i = 0; i < count; i++){
+        newWord+= sentence;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
