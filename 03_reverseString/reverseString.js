dc8e8765bc94610ab025700a0e9071789fdd8b5e
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-04 23:16:33 +1000

    Finished 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let newString = stringToReverse.split("").reverse().join("");
+    return newString;
 };
 
 // Do not edit below this line
