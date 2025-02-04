6eb2f224bef66789c6d7e6a6d0603ee873571be2
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-05 00:15:53 +1000

    Finished 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * (5 / 9);
+  var multipler = Math.pow(10, 1 || 0);
+  return Math.round(celsius * multipler) / multipler;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = celsius * (9 / 5) + 32;
+  var multipler = Math.pow(10, 1 || 0);
+  return Math.round(farenheit * multipler) / multipler;
 };
 
 // Do not edit below this line
