662b429e333782230de6f8e252410cc3afcee487
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-04 23:41:50 +1000

    Finished 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numberToRemove) {
+    let newArray = array.filter(number => !numberToRemove.includes(number));
+    return newArray;
 };
 
 // Do not edit below this line
