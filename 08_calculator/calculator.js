16fd8e0dd8c58d7c67fcd010e81f2b8fc721cd58
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-09 01:43:26 +1000

    Finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
+const add = function(...numbers) {
+  let initial = 0;
+  for (let i = 0; i < numbers.length; i++){
+    initial += numbers[i];
+  }
 	
+  return initial;
 };
 
-const subtract = function() {
+const subtract = function(...numbers) {
+  let initial = 0;
+  for (let i = 0; i < numbers.length; i++){
+    if (i === 0){
+      initial = numbers[i];
+      continue;
+    }
+    initial -= numbers[i];
+  }
 	
+  return initial;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let total = 0;
+  for (let i = 0; i < numberArray.length; i++){
+    total += numberArray[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  for (let i = 0; i < numbers.length; i++){
+    if (i === 0){
+      initial = numbers[i];
+      continue;
+    }
+    initial *= numbers[i];
+  }
+  return initial;
 };
 
-const power = function() {
-	
+const power = function(base,exponential) {
+	return base ** exponential;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0 || number === 1){
+    return 1;
+  }
+  let result = number;
+  while (number > 1){
+    number--;
+    result = result * number;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
