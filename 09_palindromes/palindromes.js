7cf8749e82e08d75852d8702fc194445f37877db
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-09 10:48:51 +1000

    Halfway through 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (palindromeString) {
+    palindromeString.toLowerCase();
+    palindromeString.filter(
+        (letter) => z
+    )
+    let reverseString = palindromeString.split('').reverse().join('').toLowerCase();;
+    for (let i = 0; i < reverseString.length; i++){
+        if (reverseString[i] !== palindromeString[i]){
+            return false;
+            break;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
