0998ed77cc308d6160e2960f9240f7453335591e
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-09 23:48:50 +1000

    finished 11 & 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleObjectArray) {
+    let sortedArray = peopleObjectArray.slice();
+    let currentYear = new Date().getFullYear();
+    sortedArray.sort((a,b) => ((a.yearOfDeath || currentYear)  - a.yearOfBirth) - ((b.yearOfDeath || currentYear)- b.yearOfBirth));
+    return sortedArray[sortedArray.length-1];
 };
 
 // Do not edit below this line
