2b0e1b545a826a560a339f4cded8d11ebc7989f5
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-09 20:13:46 +1000

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(fibonacciMember) {
+    let fibonacciSequence = [0,1,1];
+    if (fibonacciMember < 0){
+        return 'OOPS';
+    }
+    for (let i = 3; i <= fibonacciMember; i++){
+        fibonacciSequence[i] = fibonacciSequence[i-2] + fibonacciSequence[i-1];
+    }
+    return fibonacciSequence[fibonacciMember];
+};  
 
 // Do not edit below this line
 module.exports = fibonacci;
